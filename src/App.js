88bff3d35f3b923644709846917a138f3dcd6d90
aman_sharma <aman.sharma@daffodilsw.com>
2024-12-10 20:11:16 +0530

    step created for messages

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import { useState } from "react";
+
+const messages = ["hello world", "Hello Dishant", "Hello Aman"];
+
+function App() {
+  const [step, setStep] = useState(1);
+  return (
+    <>
+      <div className="steps">
+        <div className="numbers">
+          <div
+            className={`${step >= 1 ? "active" : ""} cursor-pointer`}
+            onClick={() => setStep(1)}
+          >
+            1
+          </div>
+          <div
+            className={`${step >= 2 ? "active" : ""}`}
+            onClick={() => setStep(2)}
+          >
+            2
+          </div>
+          <div
+            className={`${step >= 3 ? "active" : ""}`}
+            onClick={() => setStep(3)}
+          >
+            3
+          </div>
+        </div>
+
+        <p>Steps: {messages[step - 1]}</p>
+
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={() => setStep(step > 1 ? step - 1 : step)}
+            disabled={step <= 1}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={() => setStep(step < 3 ? step + 1 : step)}
+            disabled={step >= 3}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
